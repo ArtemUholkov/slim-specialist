ba884fb932db988adda9b88757a5dbc20a116edd
Artem <artem.ugolkov@ukr.net>
2024-06-09 09:01:08 +0300

    check

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const burger = document.querySelector('#burger');
+const popup = document.querySelector('#popup');
+const popupSlide = document.querySelector('#popupSlide');
+const sliderItem = document.querySelectorAll('.our_requirements_content_item');
+const sliderPhoto = document.querySelector('.our_requirements_img');
+const body = document.body;
+
+burger.addEventListener('click', burgerHandler);
+popup.addEventListener('click', (e) => {
+  burgerHandler(e);
+});
+
+sliderItem.forEach((e) => {
+  e.addEventListener('click', () => {
+    setImage(e, sliderItem);
+  });
+});
+
+function burgerHandler(e) {
+  if (popup.classList.contains('open')) {
+    popup.classList.add('close');
+    body.classList.remove('noscroll');
+    popupSlide.classList.add('slideout');
+
+    setTimeout(() => {
+      popup.classList.remove('close');
+      popup.classList.remove('open');
+      popupSlide.classList.remove('slideout');
+      popupSlide.classList.remove('slidein');
+    }, 300);
+  } else {
+    body.classList.add('noscroll');
+    popup.classList.add('open');
+    popupSlide.classList.add('slidein');
+  }
+  burger.classList.toggle('active');
+}
+
+var items = document.querySelectorAll('.why_item');
+
+// Add click event listener to each item
+items.forEach(function (item) {
+  var absoluteLayer = item.querySelector('.why_abs_item-text');
+
+  item.addEventListener('click', function () {
+    if (absoluteLayer.classList.contains('show')) {
+      absoluteLayer.classList.remove('show');
+    } else {
+      absoluteLayer.classList.add('show');
+    }
+  });
+});
